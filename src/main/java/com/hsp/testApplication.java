5bf4604a90aa9d181307e8012ccb4c699c23a1ba
123 <123@123.com>
2024-07-12 16:54:42 +0800

    commit branch 1.0

--- src/main/java/com/hsp/testApplication.java
+++ src/main/java/com/hsp/testApplication.java
@@ -3,5 +3,6 @@ package com.hsp;
 public class testApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World! for branch 1.0");
     }
 }
