f292f33d6cb6efd354f93069ad8bcb16562179f7
123 <123@123.com>
2024-07-12 17:05:43 +0800

    commit branch direct 1

---
+++ src/main/java/com/hsp/Hi.java
@@ -0,0 +1,11 @@
+package com.hsp;
+
+import java.util.Arrays;
+
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hi.main");
+        System.out.println("args = " + args);
+        System.out.println("args = " + Arrays.toString(args));
+    }
+}
