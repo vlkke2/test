38aae64f78f204e0ea1236c6dee1ecb74900f502
123 <123@123.com>
2024-07-12 16:56:17 +0800

    commit branch master 1

--- src/main/java/com/hsp/testApplication.java
+++ src/main/java/com/hsp/testApplication.java
@@ -3,5 +3,6 @@ package com.hsp;
 public class testApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World! branch for master");
     }
 }
